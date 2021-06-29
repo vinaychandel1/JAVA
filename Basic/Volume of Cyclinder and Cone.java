03f8e70d399fa3409054523279e81de09147bdec
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-06-29 19:46:05 +0530

    Create Volume of Cyclinder and Cone.java

---
+++ Basic/Volume of Cyclinder and Cone.java
@@ -0,0 +1,17 @@
+public class Main{  
+    public static void main(String args[])  
+    {  
+    int height=38;  
+    int radius=35;  
+    double pie=3.14285714286;  
+    double volumeofcone=pie*(radius*radius)*height/3;  
+        System.out.println("Volume of the cone="+volumeofcone); 
+    double volumeofcyclinder=pie*(radius*radius)*height;  
+    System.out.println("Volume of the cylinder="+volumeofcyclinder);  
+     }  
+}
+/*
+Volume of the cone=48766.666666711004
+Volume of the cylinder=146300.000000133
+
+*/
