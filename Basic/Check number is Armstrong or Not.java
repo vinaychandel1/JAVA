7e80045b22bd2d9c748c26923d5df08cc0d81793
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-06-29 19:46:51 +0530

    Create Check number is Armstrong or Not.java

---
+++ Basic/Check number is Armstrong or Not.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 1634, originalNumber, remainder, result = 0, n = 0;
+
+        originalNumber = number;
+
+        for (;originalNumber != 0; originalNumber /= 10, ++n);
+        System.out.println(n);
+
+        originalNumber = number;
+
+        for (;originalNumber != 0; originalNumber /= 10)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+        }
+
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+}
+/*
+4
+
+1634 is an Armstrong number.
+*/
