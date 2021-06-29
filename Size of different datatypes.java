eec4094a07edeb29149eede006e9313d01edef25
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-06-29 19:25:07 +0530

    Create Size of different datatypes.java

---
+++ Size of different datatypes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class SizeofdataTypes
+{
+ public static void main (String[] args)
+ {
+  System.out.println("Size of int: " + (Integer.SIZE/8) + " bytes.");
+  System.out.println("Size of long: " + (Long.SIZE/8) + " bytes.");
+  System.out.println("Size of char: " + (Character.SIZE/8) + " bytes.");
+  System.out.println("Size of float: " + (Float.SIZE/8) + " bytes.");
+  System.out.println("Size of double: " + (Double.SIZE/8) + " bytes.");
+  }
+}
+/*Size of int: 4 bytes.
+Size of long: 8 bytes.
+Size of char: 2 bytes.
+Size of float: 4 bytes.
+Size of double: 8 bytes.
+
+*/
